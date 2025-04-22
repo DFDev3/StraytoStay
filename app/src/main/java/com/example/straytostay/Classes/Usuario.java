293bee4d7342b2ca87f7c6813dc7522b91cb812c
DFDev3 <denis.fedi@gmail.com>
2025-04-22 13:35:50 -0500

    Usuario Multiclass implemented

--- app/src/main/java/com/example/straytostay/Classes/Usuario.java
+++ app/src/main/java/com/example/straytostay/Classes/Usuario.java
@@ -1,19 +1,34 @@
 package com.example.straytostay.Classes;
 
+import java.util.ArrayList;
+
 public class Usuario {
+    private int adminId;
+
+    // USUARIO (0)
     private String name;
+    private String imageUri;
     private String phone;
     private String citizenId;
     private String address;
     private String email;
     private String uid;
-    private int adminId;
+
+    // SHELTER (1)
+
+    private String mision;
+
+    // VETS (1)
+
+    private String nit;
+    private ArrayList<String> services;
+    private ArrayList<String> products;
+
 
     public Usuario() {
         // Required empty constructor for Firestore
     }
-
-    public Usuario(String name, String phone, String citizenId, String address, String email, int adminId, String uid) {
+    public Usuario(String name, String phone, String citizenId, String address, String email, int adminId, String uid, String imageUri) {
         this.name = name;
         this.phone = phone;
         this.citizenId = citizenId;
@@ -21,6 +36,28 @@ public class Usuario {
         this.email = email;
         this.adminId = adminId;
         this.uid = uid;
+    }
+
+    public Usuario(String name, String phone, String address, String email, int adminId, String uid, String mision, String imageUri) {
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+        this.email = email;
+        this.adminId = adminId;
+        this.uid = uid;
+        this.mision = mision;
+    }
+
+    public Usuario(String name, String phone, String address, String email, int adminId, String uid, String nit, ArrayList<String> services, ArrayList<String> products, String imageUri) {
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+        this.email = email;
+        this.adminId = adminId;
+        this.uid = uid;
+        this.nit = nit;
+        this.services = services;
+        this.products = products;
     }
 
     public String getUid() {
